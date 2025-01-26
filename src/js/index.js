c24628eef6f81cc48ea2a2c403fb8729d8d59616
José M. <51032540+JoseMirandaNeto@users.noreply.github.com>
2025-01-26 13:13:20 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,61 @@
-const btnAvancar = document.getElementById("btn-avancar");
+/*
+
+OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo cartao da lista
+ - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
+ - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+ - passo 3 - fazer aparecer o próximo cartão da lista
+  - passo 4 - buscar o cartão que está selecionado e esconder
+
+  OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista
+   - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+    - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
+     - passo 3 - fazer aparecer o cartão anterior da lista
+      - passo 4 - buscar o cartão que esta selecionado e esconder
+
+
+
+
+  */
+
+// OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo cartao da lista
+
+// passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
+const btnAvancar = document.getElementById("btn-avancar")
 const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+// passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
 btnAvancar.addEventListener("click", function () {
-  if (cartaoAtual === cartoes.length - 1) return;
+    if (cartaoAtual === cartoes.length - 1) return;
 
-  esconderCartaoSelecionado();
 
-  cartaoAtual++;
-  mostrarCartao(cartaoAtual);
+    //passo 4 - buscar o cartão que está selecionado e esconder
+    esconderCartaoSelecionado();
+
+    // passo 3 - fazer aparecer o próximo cartão da lista
+    cartaoAtual++
+    mostrarCartao(cartaoAtual);
 });
 
-btnVoltar.addEventListener("click", function () {
-  if (cartaoAtual === 0) return;
+   
+      btnVoltar.addEventListener("click", function () {
+    if (cartaoAtual === 0) return;
 
-  esconderCartaoSelecionado();
 
-  cartaoAtual--;
-  mostrarCartao(cartaoAtual);
+
+    esconderCartaoSelecionado();
+
+   
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
 });
 
 function mostrarCartao(cartaoAtual) {
-  cartoes[cartaoAtual].classList.add("selecionado");
+    cartoes[cartaoAtual].classList.add("selecionado");
 }
 
 function esconderCartaoSelecionado() {
-  const cartaoSelecionado = document.querySelector(".selecionado");
-  cartaoSelecionado.classList.remove("selecionado");
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
 }
